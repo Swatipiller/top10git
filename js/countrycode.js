8230edb08ef4befd454219eddd13a79ca76c9e4a
Swati Jha <meghana.pillersoft@gmail.com>
2023-03-21 16:41:22 +0530

    fixed numberpicker

--- js/countrycode.js
+++ js/countrycode.js
@@ -1,18 +1,51 @@
 // to get full international number use getNumber() method: e.g., iti.getNumber()
 // https://github.com/jackocnr/intl-tel-input
-var input = document.querySelector("#phone");
-var iti = window.intlTelInput(input, {
-  formatOnDisplay: true,
-  initialCountry: "nl",
-  nationalMode: true,
-  utilsScript: "../intl-tel-input/js/utils.js",
+// var input = document.querySelector("#phone");
+// var iti = window.intlTelInput(input, {
+//   formatOnDisplay: true,
+//   initialCountry: "nl",
+//   nationalMode: true,
+//   utilsScript: "../intl-tel-input/js/utils.js",
+// });
+
+var input = document.querySelectorAll("#phone");
+
+input.forEach(function (el) {
+  window.intlTelInput(el, {
+    formatOnDisplay: true,
+    initialCountry: "nl",
+    nationalMode: true,
+    utilsScript: "../intl-tel-input/js/utils.js",
+  });
 });
-var input = document.querySelector("#phone1");
-var iti = window.intlTelInput(input, {
-  formatOnDisplay: true,
-  initialCountry: "nl",
-  nationalMode: true,
-  utilsScript: "../intl-tel-input/js/utils.js",
-});
+
+// var iti = window.intlTelInput(input, {
+//   formatOnDisplay: true,
+//   initialCountry: "nl",
+//   nationalMode: true,
+//   utilsScript: "../intl-tel-input/js/utils.js",
+// });
+
+// var input = document.querySelector("#phone1");
+// var iti = window.intlTelInput(input, {
+//   formatOnDisplay: true,
+//   initialCountry: "nl",
+//   nationalMode: true,
+//   utilsScript: "../intl-tel-input/js/utils.js",
+// });
+// var input = document.querySelector("#phone2");
+// var iti = window.intlTelInput(input, {
+//   formatOnDisplay: true,
+//   initialCountry: "nl",
+//   nationalMode: true,
+//   utilsScript: "../intl-tel-input/js/utils.js",
+// });
+// var input = document.querySelector("#phone3");
+// var iti = window.intlTelInput(input, {
+//   formatOnDisplay: true,
+//   initialCountry: "nl",
+//   nationalMode: true,
+//   utilsScript: "../intl-tel-input/js/utils.js",
+// });
 
 // var number = iti.getNumber();
